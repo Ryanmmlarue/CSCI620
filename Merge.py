a1d61456d33b857a63200f2991d16935550ba9cc
catepiller <92127055+catepiller@users.noreply.github.com>
2022-10-17 17:13:00 -0400

    Remove duplicate ids

--- Merge.py
+++ Merge.py
@@ -13,7 +13,7 @@ table4 = []
 table5 = []
 table6 = []
 
-root = DIRECTORY_ROOT
+root = 'output'
 pattern1 = "*buys_a.tsv"
 pattern2 = "*gives_a.tsv"
 pattern3 = "*has_a.tsv"
@@ -58,33 +58,37 @@ for x in table1:
     concate_data1 = pd.concat(table1)
     concate_data1.head()
 
-concate_data1.to_csv(buys_a_OUTPUT_FILE, sep="\t", index=False)
+concate_data1.to_csv("merged/buys_a.tsv", sep="\t", index=False)
 
 for x in table2:
     concate_data2 = pd.concat(table2)
     concate_data2.head()
 
-concate_data2.to_csv(gives_a_OUTPUT_FILE, sep="\t", index=False)
+concate_data2.to_csv("merged/gives_a.tsv", sep="\t", index=False)
 
 for x in table3:
     concate_data3 = pd.concat(table3)
     concate_data3.head()
 
-concate_data3.to_csv(has_a_OUTPUT_FILE, sep="\t", index=False)
+concate_data3.to_csv("merged/has_a.tsv", sep="\t", index=False)
 
 for x in table4:
     concate_data4 = pd.concat(table4)
+    concate_data4.head()
 
-concate_data4.to_csv(product_OUTPUT_FILE, sep="\t", index=False)
+concate_data4 = concate_data4.drop_duplicates(subset=["product_id"], keep='first')
+concate_data4.to_csv("merged/product.tsv", sep="\t", index=False)
 
 for x in table5:
     concate_data5 = pd.concat(table5)
     concate_data5.head()
 
-concate_data5.to_csv(review_OUTPUT_FILE, sep="\t", index=False)
+concate_data5 = concate_data5.drop_duplicates(subset=["review_id"], keep='first')
+concate_data5.to_csv("merged/review.tsv", sep="\t", index=False)
 
 for x in table6:
     concate_data6 = pd.concat(table6)
     concate_data6.head()
 
-concate_data6.to_csv(user_OUTPUT_FILE, sep="\t", index=False)
+concate_data6 = concate_data6.drop_duplicates(subset=["user_id"], keep='first')
+concate_data6.to_csv("merged/user.tsv", sep="\t", index=False)
