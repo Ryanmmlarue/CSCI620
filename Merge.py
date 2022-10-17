be1b834acbd1e35426e1045c9a2100b95441846e
catepiller <92127055+catepiller@users.noreply.github.com>
2022-10-17 15:54:56 -0400

    Loops through directory

--- Merge.py
+++ Merge.py
@@ -3,30 +3,88 @@ author: Ryan Jiau; rsj7519
 """
 
 import pandas as pd
+import os
+from fnmatch import fnmatch
 
-data1 = pd.read_table(FilePath)
-data1.head()
-tables.append(data1)
+table1 = []
+table2 = []
+table3 = []
+table4 = []
+table5 = []
+table6 = []
 
-data2 = pd.read_table(FilePath)
-data2.head()
-tables.append(data2)
+root = DIRECTORY_ROOT
+pattern1 = "*buys_a.tsv"
+pattern2 = "*gives_a.tsv"
+pattern3 = "*has_a.tsv"
+pattern4 = "*product.tsv"
+pattern5 = "*review.tsv"
+pattern6 = "*user.tsv"
 
-data3 = pd.read_table(FilePath)
-data3.head()
-tables.append(data3)
+for path, subdirs, files in os.walk(root):
+    for name in files:
+        if fnmatch(name, pattern1):
+            xpath = os.path.join(path, name)
+            data = pd.read_table(xpath)
+            data.head()
+            table1.append(data)
+        elif fnmatch(name, pattern2):
+            xpath = os.path.join(path, name)
+            data = pd.read_table(xpath)
+            data.head()
+            table2.append(data)
+        elif fnmatch(name, pattern3):
+            xpath = os.path.join(path, name)
+            data = pd.read_table(xpath)
+            data.head()
+            table3.append(data)
+        elif fnmatch(name, pattern4):
+            xpath = os.path.join(path, name)
+            data = pd.read_table(xpath)
+            data.head()
+            table4.append(data)
+        elif fnmatch(name, pattern5):
+            xpath = os.path.join(path, name)
+            data = pd.read_table(xpath)
+            data.head()
+            table5.append(data)
+        elif fnmatch(name, pattern6):
+            xpath = os.path.join(path, name)
+            data = pd.read_table(xpath)
+            data.head()
+            table6.append(data)
 
-data4 = pd.read_table(FilePath)
-data4.head()
-tables.append(data4)
+for x in table1:
+    concate_data1 = pd.concat(table1)
+    concate_data1.head()
 
-data5 = pd.read_table(FilePath)
-data5.head()
-tables.append(data5)
+concate_data1.to_csv(buys_a_OUTPUT_FILE, sep="\t", index=False)
 
-for x in tables:
-    concate_data = pd.concat(tables)
-    concate_data.head()
+for x in table2:
+    concate_data2 = pd.concat(table2)
+    concate_data2.head()
 
-concate_data.to_csv(OutputFilePath, sep="\t", index=False)
-print(concate_data)
+concate_data2.to_csv(gives_a_OUTPUT_FILE, sep="\t", index=False)
+
+for x in table3:
+    concate_data3 = pd.concat(table3)
+    concate_data3.head()
+
+concate_data3.to_csv(has_a_OUTPUT_FILE, sep="\t", index=False)
+
+for x in table4:
+    concate_data4 = pd.concat(table4)
+
+concate_data4.to_csv(product_OUTPUT_FILE, sep="\t", index=False)
+
+for x in table5:
+    concate_data5 = pd.concat(table5)
+    concate_data5.head()
+
+concate_data5.to_csv(review_OUTPUT_FILE, sep="\t", index=False)
+
+for x in table6:
+    concate_data6 = pd.concat(table6)
+    concate_data6.head()
+
+concate_data6.to_csv(user_OUTPUT_FILE, sep="\t", index=False)
