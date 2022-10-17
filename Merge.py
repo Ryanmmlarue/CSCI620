b430cd523226daf52db14607d30b96adf193a032
catepiller <92127055+catepiller@users.noreply.github.com>
2022-10-17 15:14:44 -0400

    Concate a list
    Now concates a list of tables instead of hard coding concate.

--- Merge.py
+++ Merge.py
@@ -6,21 +6,27 @@ import pandas as pd
 
 data1 = pd.read_table(FilePath)
 data1.head()
+tables.append(data1)
 
 data2 = pd.read_table(FilePath)
 data2.head()
+tables.append(data2)
 
 data3 = pd.read_table(FilePath)
 data3.head()
+tables.append(data3)
 
 data4 = pd.read_table(FilePath)
 data4.head()
+tables.append(data4)
 
 data5 = pd.read_table(FilePath)
 data5.head()
+tables.append(data5)
 
-concate_data = pd.concat([data1,data2,data3,data4,data5])
-concate_data.head()
+for x in tables:
+    concate_data = pd.concat(tables)
+    concate_data.head()
 
 concate_data.to_csv(OutputFilePath, sep="\t", index=False)
 print(concate_data)
