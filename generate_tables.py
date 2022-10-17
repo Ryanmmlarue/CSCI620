865404b3efc543baf54db0e2dc1b2c48ff60c110
Ryan LaRue <28721348+Ryanmmlarue@users.noreply.github.com>
2022-10-16 20:32:44 -0400

    updated script for bulk processing

--- generate_tables.py
+++ generate_tables.py
@@ -3,6 +3,20 @@ author: Ryan LaRue; rml5169
 """
 
 import pandas as pd
+import time
+
+import os
+
+def make_directory(dir_name):
+    dir = os.path.dirname(__file__)
+    path = os.path.join(dir, 'output', dir_name)
+    os.mkdir(path)
+    return path
+
+def extract_path(file_name):
+    tokens = file_name.split('.')
+    file = tokens[1].split('/')
+    return file[-1]
 
 def generate_tables(file_name):
     df = pd.read_csv(file_name, sep='\t', on_bad_lines='skip')
@@ -20,32 +34,50 @@ def generate_tables(file_name):
     df = df.replace({'verified_purchase': {'Y': 1, 'N': 0}})
     df = df.replace({'vine': {'Y': 1, 'N': 0}})
 
+    name = extract_path(file_name)
+
+    path = make_directory(name)
+
     # create User table
     user = df['user_id'].drop_duplicates()
-    user.to_csv('./output/user.tsv', sep='\t', index=False)
+    user.to_csv(path + '/user.tsv', sep='\t', index=False)
 
     # create Review table
     review = df[['review_id', 'star_rating', 'helpful_votes', 'total_votes',
                  'vine', 'verified_purchase', 'headline', 'body', 'date']]
-    review.to_csv('./output/review.tsv', sep='\t', index=False)
+    review.to_csv(path + '/review.tsv', sep='\t', index=False)
 
     # create Product table
     product = df[['product_id', 'parent', 'title', 'category']].drop_duplicates()
-    product.to_csv('./output/product.tsv', sep='\t', index=False)
+    product.to_csv(path + '/product.tsv', sep='\t', index=False)
 
     # create GivesA table
     gives_a = df[['user_id', 'review_id']]
-    gives_a.to_csv('./output/gives_a.tsv', sep='\t', index=False)
+    gives_a.to_csv(path + '/gives_a.tsv', sep='\t', index=False)
 
     # create HasA table
     has_a = df[['review_id', 'product_id']]
-    has_a.to_csv('./output/has_a.tsv', sep='\t', index=False)
+    has_a.to_csv(path + '/has_a.tsv', sep='\t', index=False)
 
     # create BuysA table
     buys_a = df[['user_id', 'product_id']]
-    buys_a.to_csv('./output/buys_a.tsv', sep='\t', index=False)
+    buys_a.to_csv(path + '/buys_a.tsv', sep='\t', index=False)
 
 
-file_path = 'amazon_reviews_multilingual_US_v1_00.tsv'
+def process_file(file_path):
+    start = time.time()
+    generate_tables('./data/' + file_path)
+    end = time.time()
+    print('Processed', file_path, 'after', end-start, 'seconds')
 
-generate_tables('./data/' + file_path)
+
+tsvs = os.listdir('./data')
+
+for tsv in tsvs:
+    process_file(tsv)
+
+
+
+
+
+
