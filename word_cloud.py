daf03c410e76b95388b2dccb806b2829f8f86240
Ritesh091 <46635023+Ritesh091@users.noreply.github.com>
2022-11-28 10:38:22 -0500

    Updated Wordcloud file

--- word_cloud.py
+++ word_cloud.py
@@ -1,18 +1,33 @@
 from google.colab import drive
-drive.mount('/content/gdrive')
-
 import pandas as pd
 import os
+from nltk.corpus import stopwords
+import nltk
+import re
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from collections import Counter
+
+drive.mount('/content/gdrive')
+stop = stopwords.words('english')
+
 dir = '/content/gdrive/Shareddrives/CSCI 620/Project/output/'
 files = os.listdir(dir)
 review= []
 for file_ in files:
-  data = pd.read_csv(dir + file_ + '/review.tsv', index_col = False, sep="\t")
+  data = pd.read_csv(dir + file_ + '/review.tsv', index_col = False, sep="\t", skipinitialspace=True)
+  data = data[data['body'].notna()]
+  data['body'] = data['body'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
   review.append(data['body'].values)
 
 flat_list = [item for sublist in review for item in sublist]
 
-from collections import Counter
+text = (' ').join(flat_list)
+
+text = re.sub('<[^<]+?>', '', text)
+
+flat_list = text.split()
+
 word_could_dict=Counter(flat_list)
 wordcloud = WordCloud(width = 1000, height = 500).generate_from_frequencies(word_could_dict)
 
